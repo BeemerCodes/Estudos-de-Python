fdee8e817e02d4f8561aa4b39b5fb15fd3b3e9ea
BeemerCodes <detonainyoutub@gmail.com>
2020-06-16 16:51:22 -0300

    modificado e parcialmente terminado

--- Valor-bebidas.py
+++ Valor-bebidas.py
@@ -1,36 +1,21 @@
-porcao = 0
-valor = 0
-energetico = 0
-gelo = 0
-tot1 = 0
-totalg = 0
-print("[1]Chanceler  [2]Passaporte")
-tipowhisky = int(input("Qual o Whisky? "))
-quantidade = float(input("Quantos litros são de whisky? "))
-if tipowhisky == 1:
-    valor = 15
-else:
-    valor = 40
-if valor == 15:
-    porcao = 6
-else:
-    porcao = 10
-if porcao == 6:
-    energetico = 2400
-else:
-    energetico = 4000
-if energetico == 2400:
-    gelo = 12
-else:
-    gelo = 20
-tot1 = valor + gelo
-if porcao == 6:
-    totalg = 6
-else:
-    totalg = 10
+print("-=-"*20)
+print("       C a l c u l a d o r a     d e     g o r ó      ")
+valorw = float(input("Qual o valor do whisky? "))
+litros = int(input("Quantos litros? "))
+gelo = 5
+valorg = 2
+gelot = (litros * gelo)
+valorgt = litros * valorg
+print("Serão usados", gelot, "gelos")
+print("Fará", gelot, "porções!")
+print("OBS: Fara {} porções de 200 de whisky".format(gelot))
+energv = 8
+energ = 1
+energt = litros * energ
+energvt = energt * energv
+print("você precisara de {}L de energetico".format(energt))
+valort = (litros * valorw) + valorgt + energvt
+print("Terá valor total de", valort)
 
-print("Sera usado {} Gelos de coco ou maracujá.".format(int (totalg * quantidade)))
-print("Sera usado {}L de whisky!".format(quantidade))
-print("Sera usado {}L de energetico!".format(energetico))
-print("Serão feitas {} porções de bebida!".format(int (porcao * quantidade)))
-print("O valor total sera de R${:.2f}".format(tot1 * quantidade))
+
+
